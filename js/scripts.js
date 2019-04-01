4e1b03b7a921d30e3ed95ae2029a2cbd3f268d69
Alexander Kondra and Mustafa Ghasemi <mustafa@>
2019-04-01 13:19:35 -0700

    add initial file and get the infor from array and print them on the screen

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+  var iceCreamFlavors = ["mango","vanilla","berries"];
+  iceCreamFlavors.forEach(function(flavors) {
+    document.write(flavors + "<br>");
+  });
+
+// });
